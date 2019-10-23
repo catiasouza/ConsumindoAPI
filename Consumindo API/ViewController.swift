16a6a9904796f7b8f05093eea7433c39e30d6b25
Catia Miranda de Souza <catiamiranda@NBBV030098.local>
2019-10-23 14:02:33 -0300

    Finalizando projeto

--- Consumindo API/ViewController.swift
+++ Consumindo API/ViewController.swift
@@ -3,9 +3,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var precoBitcoin: UILabel!
+    
+    @IBOutlet weak var botaoAtualizar: UIButton!
+    
+    @IBAction func atualizarPreco(_ sender: Any) {
+        
+        self.recuperarPrecoBiticoins()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.recuperarPrecoBiticoins()
+      
+    }
+    func formatarPreco(preco: NSNumber) -> String{
+        
+        let nf = NumberFormatter()
+        nf.numberStyle = .decimal
+        nf.locale = Locale(identifier: "pt_BR")
+        
+        if let precoFinal = nf.string(from: preco) {
+            return precoFinal
+        }
+        return "0,00"
+    }
+    func recuperarPrecoBiticoins(){
+        
+        self.botaoAtualizar.setTitle("Atualizando...", for: .normal)
         
         if  let url = URL(string: "https://blockchain.info/ticker"){
             let tarefa = URLSession.shared.dataTask(with: url) { (dados, requisicao, erro) in
@@ -17,29 +45,31 @@ class ViewController: UIViewController {
                         do {
                             if let objetoJson =  try JSONSerialization.jsonObject(with: dadosRetorno, options: []) as? [String: Any]{
                                 if let brl = objetoJson["BRL"] as? [String: Any]{
-                                
-                                    if let preco = brl["buy"] as? Float{
-                                        print(preco)
+                                    
+                                    if let preco = brl["buy"] as? Double{
+                                        let precoFormatado = self.formatarPreco(preco: NSNumber(value: preco))
+                                        //DENTRO DE UMA CLOGERE PARA ATUALIZAR A INTERFACE E NECESSARIO USAR
+                                        DispatchQueue.main.async(execute: {
+                                             self.precoBitcoin.text = "R$ " + precoFormatado
+                                              self.botaoAtualizar.setTitle("Atualizar", for: .normal)
+                                        })
+                                       
                                     }
                                 }
-                                
-                                
+                         
+                            }
+                            
+                            
+                              } catch {
+                                  print("Erro ao formatar retorno")
+                              }
                           }
-                            
-                            
-                        } catch {
-                            print("Erro ao formatar retorno")
-                        }
-                    }
-                }else{
-                    print("Erro ao fazer a consulta")
-                }
-                
-            }
-            tarefa.resume()
-        }
-    }
-    
-
+                      }else{
+                          print("Erro ao fazer a consulta")
+                      }
+                      
+                  }
+                  tarefa.resume()
+              }    }
 
 }
