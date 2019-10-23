e26dbf120d6e42b945a67a4cd1ac53ab2aff1414
Catia Miranda de Souza <catiamiranda@NBBV030098.local>
2019-10-23 11:28:22 -0300

    Recuperando objetos

--- Consumindo API/ViewController.swift
+++ Consumindo API/ViewController.swift
@@ -8,10 +8,29 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         if  let url = URL(string: "https://blockchain.info/ticker"){
-             let tarefa = URLSession.shared.dataTask(with: url) { (dados, requisicao, erro) in
-                       
+            let tarefa = URLSession.shared.dataTask(with: url) { (dados, requisicao, erro) in
+                
                 if erro == nil {
-                    print("Sucesso ao fazer consulta")
+                    
+                    if let dadosRetorno = dados{
+                        
+                        do {
+                            if let objetoJson =  try JSONSerialization.jsonObject(with: dadosRetorno, options: []) as? [String: Any]{
+                                if let brl = objetoJson["BRL"] as? [String: Any]{
+                                
+                                    if let preco = brl["buy"] as? Float{
+                                        print(preco)
+                                    }
+                                }
+                                
+                                
+                          }
+                            
+                            
+                        } catch {
+                            print("Erro ao formatar retorno")
+                        }
+                    }
                 }else{
                     print("Erro ao fazer a consulta")
                 }
